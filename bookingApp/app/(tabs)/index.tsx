52d7f059cae18bd62a297bd5b10af21931d46e58
Taimur Hasan <70123599+tshasan@users.noreply.github.com>
2024-10-30 15:02:37 -0700

    initialize tamagui.dev

--- bookingApp/app/(tabs)/index.tsx
+++ bookingApp/app/(tabs)/index.tsx
@@ -1,66 +1,47 @@
+import { useFonts } from 'expo-font';
+import { TamaguiProvider } from 'tamagui';
+import config from '../../tamagui.config';
 import { useState, useEffect } from 'react';
 import { supabase } from '../../utils/supabase';
-import Auth from '../../components/Auth';
+import LoginPage from './LoginPage';
 import Account from '../../components/Account';
 import { View, Platform } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Session } from '@supabase/supabase-js';
 
 export default function App() {
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  });
+
   const [session, setSession] = useState<Session | null>(null);
-  const [isReady, setIsReady] = useState(false);
 
   useEffect(() => {
-    const getSession = async () => {
-      try {
-        if (Platform.OS === 'web' && typeof window === 'undefined') return;
-        
-        if (Platform.OS === 'web') {
-          console.log('web');
-          const { data: { session } } = await supabase.auth.getSession();
-          setSession(session);
-        } else {
-          console.log('mobile');
-          const storedSession = await AsyncStorage.getItem('supabase.auth.token');
-          if (storedSession) {
-            const parsedSession = JSON.parse(storedSession);
-            setSession(parsedSession);
-          } else {
-            const { data: { session } } = await supabase.auth.getSession();
-            setSession(session);
-          }
-        }
-        setIsReady(true);
-      } catch (error) {
-        console.error('Error retrieving session:', error);
-      }
-    };
-
-    getSession();
-
-    const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
       setSession(session);
-      if (Platform.OS !== 'web') {
-        if (session) {
-          AsyncStorage.setItem('supabase.auth.token', JSON.stringify(session));
-        } else {
-          AsyncStorage.removeItem('supabase.auth.token');
-        }
-      }
     });
 
-    return () => {
-      subscription.unsubscribe();
-    };
+    const { data: { subscription } } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setSession(session);
+      }
+    );
+
+    return () => subscription.unsubscribe();
   }, []);
 
-  if (!isReady) {
-    return null; 
+  if (!loaded) {
+    return null;
   }
 
   return (
-    <View>
-      {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
-    </View>
+    <TamaguiProvider config={config}>
+      {session && session.user ? (
+        <Account key={session.user.id} session={session} />
+      ) : (
+        <loginPage />
+      )}
+    </TamaguiProvider>
   );
 }
--- bookingApp/app/+html.tsx
+++ bookingApp/app/+html.tsx
@@ -1,3 +1,5 @@
+import { TamaguiProvider } from 'tamagui';
+import tamaguiConfig from '@tamagui/config';
 import { ScrollViewStyleReset } from 'expo-router/html';
 import { type PropsWithChildren } from 'react';
 
@@ -11,29 +13,24 @@ export default function Root({ children }: PropsWithChildren) {
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
 
-        {/*
-          Disable body scrolling on web. This makes ScrollView components work closer to how they do on native.
-          However, body scrolling is often nice to have for mobile web. If you want to enable it, remove this line.
-        */}
-        <ScrollViewStyleReset />
-
-        {/* Using raw CSS styles as an escape-hatch to ensure the background color never flickers in dark-mode. */}
-        <style dangerouslySetInnerHTML={{ __html: responsiveBackground }} />
-        {/* Add any additional <head> elements that you want globally available on web... */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
       </head>
-      <body>{children}</body>
+      <body>
+        <TamaguiProvider config={tamaguiConfig}>
+          <ScrollViewStyleReset />
+          {children}
+        </TamaguiProvider>
+      </body>
     </html>
   );
 }
-
-const responsiveBackground = `
-body {
-  background-color: #fff;
-}
-@media (prefers-color-scheme: dark) {
-  body {
-    background-color: #000;
-  }
-}`;
